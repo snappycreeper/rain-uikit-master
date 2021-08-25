90d24b6a748e8d443b210826f7dd345cb9e96f33
snappyswap <anhochanadel@gmail.com>
2021-08-24 19:13:00 -0500

    colors

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2748,9 +2748,9 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#aa8929",
-    primaryBright: "#aa8929",
-    primaryDark: "#aa8929",
+    primary: "#7c12a6",
+    primaryBright: "#7c12a6",
+    primaryDark: "#7c12a6",
     secondary: "#7645D9",
     success: "#31D0AA",
     warning: "#FFB237",
