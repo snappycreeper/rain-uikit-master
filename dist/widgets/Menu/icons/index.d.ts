e5db2056b99e1c3c594b3e24e0821378e9a6951a
snappycreeper <anhochanadel@gmail.com>
2021-09-05 18:30:38 -0500

    Revert "wheel"
    This reverts commit 26f00eed28029f100b5874131b9e239631374dbb.

--- dist/widgets/Menu/icons/index.d.ts
+++ dist/widgets/Menu/icons/index.d.ts
@@ -25,4 +25,3 @@ export { default as RoadmapIcon } from "./Roadmap";
 export { default as BondsIcon } from "./Bonds";
 export { default as VaultsIcon } from "./Vaults";
 export { default as EggHouseIcon } from "./EggHouse";
-export { default as MoneyWheelIcon } from "./MoneyWheel";
